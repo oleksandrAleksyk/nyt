1f01caa58d09410c5fd6e9c1847cbc27251260fa
unknown <oleksandr.aleksyk@gmail.com>
2020-10-09 20:37:28 +0300

    Updates

---
+++ precache-manifest.f427719ea13c7c66cdd286bf21fd075d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5490c1e010e5fec13b1e6902e602553",
+    "url": "/nyt/index.html"
+  },
+  {
+    "revision": "8493d928a71b85161881",
+    "url": "/nyt/static/css/main.d4ddc302.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/nyt/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nyt/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8493d928a71b85161881",
+    "url": "/nyt/static/js/main.f46783a2.chunk.js"
+  },
+  {
+    "revision": "f350b31e4f6a4d6adcc7",
+    "url": "/nyt/static/js/runtime-main.5f9f9c51.js"
+  },
+  {
+    "revision": "e09902d3c01aebe973370a6d2d15afbd",
+    "url": "/nyt/static/media/NYTlogo.e09902d3.jpg"
+  }
+]);
